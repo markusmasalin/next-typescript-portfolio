f88cb0b46847e6f866b5e1c31061dc477fbe0a97
markusmasalin <markus.masalin@houston-inc.com>
2023-01-26 22:24:43 +0200

    Change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { NextPage } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
       <Head>
@@ -121,3 +122,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Home;
