3e73f126dfd846abb0bc89ca16b83042c5ca8dc5
markusmasalin <markus.masalin@houston-inc.com>
2023-02-23 15:53:02 +0200

    Add wrapper div above page content

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,26 @@
 import { ReactNode } from 'react'
+import styled from 'styled-components'
 import Footer from './footer/Footer'
 import NavBar from './header/NavBar'
 
 
 type Props = {
     children?: ReactNode
-  }
+}
+
+const ContentDiv = styled.div`
+  min-height: 800px;
+`
   
 const Layout = ({ children }: Props) => {
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <ContentDiv>  
+        <main>{children}</main>
+
+      </ContentDiv>
+     
       <Footer />
     </>
   )
