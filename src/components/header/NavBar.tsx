7384579167ebce3c0fcd83cb8d64b118c5291597
markusmasalin <markus.masalin@houston-inc.com>
2023-02-23 15:49:38 +0200

    Add mobile icon svgs in same logic

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,17 +1,39 @@
 import { NextPage } from 'next';
 import styled from 'styled-components';
-import NavLink, { NavLinkProps } from './NavLink';
-import RightNav from './RightNav';
+import { NavLinkProps } from './NavLink';
+import RightNav from './MobileMenu';
+import { Hamburger } from '../../assets/index'
+import { useState } from 'react';
+import DesktopMenu from './DesktopMenu';
+import { CloseIcon } from '../../assets/index'
 
 const NavContainer = styled.header`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
   padding: 20px;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 
+const HamburgerIcon = styled(Hamburger)`
+  height: 50px;
+  width: 50px;
+`
+const MobileIconDiv = styled.div`
+  @media (min-width: 600px) {
+    display: none
+  } 
+  cursor: pointer;
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 10px;
+  z-index: 20;
+`
+
 const NavBar:NextPage = () => {
+
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
   const navList: NavLinkProps[] = [
     {
@@ -35,16 +57,21 @@ const NavBar:NextPage = () => {
       href: '/contact' 
     }
   ]
-
   return (
     <>
       <NavContainer>
-        {navList.map(navlink => {
-          return <NavLink key={navlink.name} name={navlink.name} href={navlink.href} />
-        })}
-     
+        <p> Markus Masalin
+        </p>
+        {mobileNavOpen && <RightNav navList={navList} setNavOpen={setMobileNavOpen} />}
+
+        <DesktopMenu navList={navList}/>
+       
+        <MobileIconDiv onClick={() => setMobileNavOpen((c => !c))}>
+          { mobileNavOpen ? <CloseIcon /> : <HamburgerIcon /> }
+        </MobileIconDiv>
+
+       
       </NavContainer>
-      <RightNav />
     </>
   );
 }
