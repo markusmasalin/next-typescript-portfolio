998148cae74cdabec71814963ad7285c0010172d
markusmasalin <markus.masalin@houston-inc.com>
2023-02-23 15:54:19 +0200

    Create stacked elements in Homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,48 @@
 import { NextPage } from 'next'
+import Image from 'next/image';
+import styled from 'styled-components';
+import { PersonalPhoto } from '../assets/index';
+
+const PageContainer = styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    min-width: 400px;
+    margin: 0 auto;
+  }
+  
+`
+const HalfScreenDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+
+const JumboTextDiv = styled.div`
+  font-size: 40px;
+ 
+`
+
 
 
 
 const Homepage :NextPage = () => {
   return (
-    <div>
-      <p> Homepage
-      </p>
-    </div>
+    <PageContainer>
+      <HalfScreenDiv>
+        <JumboTextDiv>
+          <p>Scrum Master</p>
+          <p>Agile Coach</p>
+          <p>Fullstack Developer</p>
+        </JumboTextDiv>
+
+      </HalfScreenDiv>
+      <HalfScreenDiv>
+        <Image src={PersonalPhoto} alt={''} />
+      </HalfScreenDiv>
+
+    </PageContainer>
   ) 
 }
 
